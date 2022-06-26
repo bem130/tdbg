979ecdfee072deeeecd4c6b17e6f1f29b19988b5
haruk130 <haruki070617@gmail.com>
2022-06-26 20:16:30 +0900

    make 3d world

--- makeworld.js
+++ makeworld.js
@@ -1,112 +1,52 @@
 
     var blks = []
-    function makeworld(x,y) { // size x,y
-        blks = Array.from(Array(y*2+1),_=>Array(x*2+1).fill(0));
-        blks[Math.floor(Math.random()*y)*2+1][Math.floor(Math.random()*x)*2+1] = 1
-        while (searchwall(x,y)) {
-            for (let ly=0;ly<y;ly++) {
-                for (let lx=0;lx<x;lx++) {
-                    ra = searcharound(lx,ly,x,y);
-                    makenewroad(lx,ly,ra);
-                }
-            }
+    function makeworld(x,y,z) { // size x,y,z
+        blks = Array.from(Array(y),_=>Array.from(Array(x),_=>Array(z).fill(1)));
+        console.log(blks)
+        for (i=0;i<20;i++) {
+            blks[Math.floor(Math.random()*y)][Math.floor(Math.random()*x)][Math.floor(Math.random()*z)] = 0
         }
-    }
-    function searchwall(x,y) {
-        for (let ly=0;ly<y;ly++) {
-            for (let lx=0;lx<x;lx++) {
-                if (blks[ly*2+1][lx*2+1]==0) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    function searcharound(cx,cy,x,y) {
-        ax = cx*2+1;
-        ay = cy*2+1;
-        ra = [];
-        if (blks[ay][ax]==0) {
-            return ra;
-        }
-        if (cy>0&&blks[ay-2][ax]==0) {
-            ra.push(1);
-        }
-        if (cx>0&&blks[ay][ax-2]==0) {
-            ra.push(2);
-        }
-        if (cy<y-1&&blks[ay+2][ax]==0) {
-            ra.push(3);
-        }
-        if (cx<x-1&&blks[ay][ax+2]==0) {
-            ra.push(4);
-        }
-        return ra;
-    }
-    function makenewroad(cx,cy,ard) { // ard - around
-        if (ard.length<1) {
-            return false;
-        }
-        ax = cx*2+1;
-        ay = cy*2+1;
-
-        card = ard[Math.floor(Math.random()*ard.length)] // card - chose around
-        if (card==1) {
-            blks[ay-2][ax]=1;
-            blks[ay-1][ax]=1;
-        }
-        if (card==2) {
-            blks[ay][ax-2]=1;
-            blks[ay][ax-1]=1;
-        }
-        if (card==3) {
-            blks[ay+2][ax]=1;
-            blks[ay+1][ax]=1;
-        }
-        if (card==4) {
-            blks[ay][ax+2]=1;
-            blks[ay][ax+1]=1;
-        }
-        return true;
     }
 
     function datato3d() {
         x = blks[0].length;
         y = blks.length;
+        z = blks[0][0].length;
         scene["game"] = [[0],[]];
         fmaze = scene["game"][1];
         c = [212,210,254];
-        fc = [242,240,254];
         for (let ly=0;ly<y;ly++) {
             for (let lx=0;lx<x;lx++) {
-                if (blks[ly][lx]==0) {
-                    // 上面
-                    fmaze.push([[lx,ly,1],[lx+1,ly,1],[lx+1,ly+1,1],[0],[c[0]],[c[1]],[c[2]],[0],])
-                    fmaze.push([[lx,ly+1,1],[lx,ly,1],[lx+1,ly+1,1],[0],[c[0]],[c[1]],[c[2]],[0],])
-                    // 底面
-                    fmaze.push([[lx,ly,0],[lx+1,ly+1,0],[lx+1,ly,0],[0],[c[0]],[c[1]],[c[2]],[0],])
-                    fmaze.push([[lx,ly+1,0],[lx+1,ly+1,0],[lx,ly,0],[0],[c[0]],[c[1]],[c[2]],[0],])
-                    // 横面
-                    if (!(lx>0&&blks[ly][lx-1]==0)) {
-                        fmaze.push([[lx,ly,0],[lx,ly,1],[lx,ly+1,0],[0],[c[0]],[c[1]],[c[2]],[0],]);
-                        fmaze.push([[lx,ly,1],[lx,ly+1,1],[lx,ly+1,0],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                for(let lz=0;lz<z;lz++) {
+                    if (blks[ly][lx][lz]==0) {
+                        // 上面
+                        fmaze.push([[lx,ly,lz+1],[lx+1,ly,lz+1],[lx+1,ly+1,lz+1],[0],[c[0]],[c[1]],[c[2]],[0],])
+                        fmaze.push([[lx,ly+1,lz+1],[lx,ly,lz+1],[lx+1,ly+1,lz+1],[0],[c[0]],[c[1]],[c[2]],[0],])
+                        // 底面
+                        fmaze.push([[lx,ly,lz+0],[lx+1,ly+1,lz+0],[lx+1,ly,lz+0],[0],[c[0]],[c[1]],[c[2]],[0],])
+                        fmaze.push([[lx,ly+1,lz+0],[lx+1,ly+1,lz+0],[lx,ly,lz+0],[0],[c[0]],[c[1]],[c[2]],[0],])
+                        // 横面
+                        if (!(lx>0&&blks[ly][lx-1][lz]==0)) {
+                            fmaze.push([[lx,ly,lz+0],[lx,ly,lz+1],[lx,ly+1,lz+0],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                            fmaze.push([[lx,ly,lz+1],[lx,ly+1,lz+1],[lx,ly+1,lz+0],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                        }
+                        
+                        if (!(ly<y-1&&blks[ly+1][lx][lz]==0)) {
+                            fmaze.push([[lx,ly+1,lz+0],[lx,ly+1,lz+1],[lx+1,ly+1,lz+1],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                            fmaze.push([[lx+1,ly+1,lz+0],[lx,ly+1,lz+0],[lx+1,ly+1,lz+1],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                        }
+                        
+                        if (!(lx<x-1&&blks[ly][lx+1][lz]==0)) {
+                            fmaze.push([[lx+1,ly+1,lz+0],[lx+1,ly+1,lz+1],[lx+1,ly,lz+1],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                            fmaze.push([[lx+1,ly,lz+0],[lx+1,ly+1,lz+0],[lx+1,ly,lz+1],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                        }
+                        
+                        if (!(ly>0&&blks[ly-1][lx][lz]==0)) {
+                            fmaze.push([[lx+1,ly,lz+0],[lx+1,ly,lz+1],[lx,ly,lz+1],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                            fmaze.push([[lx,ly,lz+0],[lx+1,ly,lz+0],[lx,ly,lz+1],[0],[c[0]],[c[1]],[c[2]],[0],]);
+                        }
+    
                     }
-                    
-                    if (!(ly<y-1&&blks[ly+1][lx]==0)) {
-                        fmaze.push([[lx,ly+1,0],[lx,ly+1,1],[lx+1,ly+1,1],[0],[c[0]],[c[1]],[c[2]],[0],]);
-                        fmaze.push([[lx+1,ly+1,0],[lx,ly+1,0],[lx+1,ly+1,1],[0],[c[0]],[c[1]],[c[2]],[0],]);
-                    }
-                    
-                    if (!(lx<x-1&&blks[ly][lx+1]==0)) {
-                        fmaze.push([[lx+1,ly+1,0],[lx+1,ly+1,1],[lx+1,ly,1],[0],[c[0]],[c[1]],[c[2]],[0],]);
-                        fmaze.push([[lx+1,ly,0],[lx+1,ly+1,0],[lx+1,ly,1],[0],[c[0]],[c[1]],[c[2]],[0],]);
-                    }
-                    
-                    if (!(ly>0&&blks[ly-1][lx]==0)) {
-                        fmaze.push([[lx+1,ly,0],[lx+1,ly,1],[lx,ly,1],[0],[c[0]],[c[1]],[c[2]],[0],]);
-                        fmaze.push([[lx,ly,0],[lx+1,ly,0],[lx,ly,1],[0],[c[0]],[c[1]],[c[2]],[0],]);
-                    }
-
                 }
             }
         }
